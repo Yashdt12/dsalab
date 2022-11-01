3c116180263883876a2b9822d4871d923d0706f8
Yash Turambekar <yashdt12@icloud.com>
2022-11-02 00:00:49 +0530

    Added codes for q38 and q39

--- q38.c
+++ q38.c
@@ -1,1 +1,99 @@
 // WAP to implement stack using queue
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct queue{
+    int front;
+    int rear;
+    int array[10];
+}queue;
+typedef struct stack{
+    queue *q1;
+    queue *q2;
+}stack;
+int isFull(queue *q){
+    return (q->rear)==(sizeof(q->array)/sizeof(q->array[0])-1);
+}
+int isEmpty(queue *q){
+    return q->front==-1;
+}
+void enqueue(queue *q,int value){
+    if(isFull(q)){
+        printf("Queue is full\n");
+    }
+    else if(isEmpty(q)){
+        q->front++;
+        q->array[++q->rear]=value;
+    }
+    else{
+        q->array[++q->rear]=value;
+    }
+}
+int dequeue(queue *q){
+    int temp;
+    if(isEmpty(q)){
+        printf("Queue is empty\n");
+    }
+    else if(q->front==q->rear){
+        temp=q->array[q->front];
+        q->rear=-1;
+        q->front=-1;
+        return temp;
+    }
+    else{
+        temp=q->array[q->front++];
+        return temp;
+    }
+}
+void push(stack *s,int value){
+    if(isFull(s->q1)){
+        printf("Stack is full\n");
+    }
+    else{
+        enqueue(s->q1,value);
+    }
+}
+int pop(stack *s){
+    int temp;
+    if(isEmpty(s->q1)){
+        printf("Stack is empty\n");
+    }
+    else if(s->q1->front==s->q1->rear){
+        return dequeue(s->q1);
+    }
+    else{
+        for(int i=s->q1->front;i<s->q1->rear;i++){
+            enqueue(s->q2,dequeue(s->q1));
+        }
+        temp=dequeue(s->q1);
+        for(int i=s->q2->front;i<=s->q2->rear;i++){
+            enqueue(s->q1,dequeue(s->q2));
+        }
+        return temp;
+    }
+}
+void printStack(stack *s){
+    if(isEmpty(s->q1)){
+        printf("Stack is empty\n");
+    }
+    else{
+        printf("Elements in stack are:\n");
+        for(int i=s->q1->rear;i>=s->q1->front;i--){
+            printf("%d\n",s->q1->array[i]);
+        }     
+    }
+}
+void main(){
+    stack *s=(stack*)malloc(sizeof(stack));
+    s->q1=(queue*)malloc(sizeof(queue));
+    s->q2=(queue*)malloc(sizeof(queue));
+    s->q1->front=s->q1->rear=s->q2->front=s->q2->rear=-1;
+    pop(s);
+    push(s,1);
+    push(s,2);
+    pop(s);
+    push(s,3);
+    printStack(s);
+    free(s->q1);
+    free(s->q2);
+    free(s);
+}
--- q39.c
+++ q39.c
@@ -1,1 +1,78 @@
 // WAP to implement queue using stack
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct stack{
+    int top;
+    int array[10];
+}stack;
+typedef struct queue{
+    stack *s1;
+    stack *s2;
+}queue;
+int isFull(stack *s){
+    return (s->top==(sizeof(s->array)/sizeof(s->array[0])-1));
+}
+int isEmpty(stack *s){
+    return (s->top==-1);
+}
+void push(stack *s,int value){
+    if(isFull(s)){
+        printf("Stack is full\n");
+    }
+    else{
+        s->array[++s->top]=value;
+    }
+}
+int pop(stack *s){
+    if(isEmpty(s)){
+        printf("Stack is empty\n");
+    }
+    else{
+        return s->array[s->top--];
+    }
+}
+void enqueue(queue *q,int value){
+    if(isFull(q->s1)){
+        printf("Queue is full\n");
+    }
+    else{
+        push(q->s1,value);
+    }
+}
+int dequeue(queue *q){
+    if(isEmpty(q->s1)){
+        printf("Queue is empty\n");
+    }
+    else{
+        while(q->s1->top!=0){
+            push(q->s2,pop(q->s1));
+        }
+        int temp=pop(q->s1);
+        while(!isEmpty(q->s2)){
+            push(q->s1,pop(q->s2));
+        }
+        return temp;
+    }
+}
+void printQueue(queue *q){
+    
+    printf("Elements in the queue are: ");
+    for(int i=0;i<=q->s1->top;i++){
+        printf("%d ",q->s1->array[i]);
+    }
+    printf("\n");
+}
+void main(){
+    queue *q=(queue*)malloc(sizeof(queue));
+    q->s1=(stack*)malloc(sizeof(stack));
+    q->s2=(stack*)malloc(sizeof(stack));
+    q->s1->top=q->s2->top=-1;
+    enqueue(q,1);
+    enqueue(q,2);
+    dequeue(q);
+    enqueue(q,3);
+    printQueue(q);
+    free(q->s1);
+    free(q->s2);
+    free(q);
+}
